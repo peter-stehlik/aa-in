079df5604f5186d17434a61e3caccc93f794ad8e
Peter Stehlik <peter@inovative.sk>
2022-02-11 14:35:34 +0100

    zakl. sabl. nav.

--- wp-content/themes/wp-dashboard/html/src/assets/js/custom.js
+++ wp-content/themes/wp-dashboard/html/src/assets/js/custom.js
@@ -1,5 +1,6 @@
 let AA = {
     $menu: document.querySelector(".nav-wrap"),
+    $subMenu: document.querySelector(".sub-nav-wrap"),
 
     toggleMobNav: () => {
 		let $el = document.querySelector(".js-nav-toggler");
@@ -19,18 +20,58 @@ let AA = {
 
 			$hamburger.classList.toggle("is-opened");
 		});
-		/*
-		document.querySelector(".js-back-mob-nav").addEventListener("click", () => {
-			AA.helperOpenMobNav();
-		});*/
 	},
 	helperCloseMobNav: () => {
-		AA.$menu.classList.remove("is-ready", "is-opened");
+		AA.$menu.classList.remove("is-opened");
+		AA.$subMenu.classList.remove("is-opened");
 	},
 	helperOpenMobNav: () => {
 		AA.$menu.classList.add("is-opened");
-		AA.$menu.classList.remove("is-ready");
+		AA.$subMenu.classList.remove("is-opened");
+	},
+	buildMobNav: () => {
+		let $navLinks = document.querySelectorAll(".left-nav > li");
+		let build = $el => {
+			let $arr = `
+				<a class="js-show-sub-nav lg:hidden" href="javascript:void(0);">
+					<img src="./assets/img/arrow-full-right-medium.svg" width="30" height="20" alt="arrow white right">
+				</a>
+			`;
+			let subNav = $el.querySelector("ul");
+
+			if( subNav ){
+				$el.innerHTML += $arr;
+			}
+		};
+
+		$navLinks.forEach( el => build(el) );
+
+		AA.buildMobSubNav();
+	},
+	buildMobSubNav: () => {
+		let $navLinks = document.querySelectorAll(".js-show-sub-nav");
+		let build = $el => {
+			$el.addEventListener("click", () => {
+				let $subNav = $el.closest("li").querySelector("ul").innerHTML;
+
+				document.querySelector(".js-mob-sub-nav").innerHTML = $subNav;
+
+				console.log( $subNav );
+
+				AA.$subMenu.classList.add("is-opened");
+			});
+		};
+
+		$navLinks.forEach( el => build(el) );
+
+		document.querySelector(".js-back-mob-nav").addEventListener("click", () => {
+			AA.$subMenu.classList.remove("is-opened");
+		});
 	},
 }
 
 AA.toggleMobNav();
+
+if( window.innerWidth < 1024 ){
+	AA.buildMobNav();
+}
