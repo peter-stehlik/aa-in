9ac79e945eaca67fd90593f81ffccd04ca5b0775
Peter Stehlik <peter@inovative.sk>
2022-02-11 09:54:53 +0100

    pc verzia aktualit

--- wp-content/themes/wp-dashboard/html/src/assets/js/custom.js
+++ wp-content/themes/wp-dashboard/html/src/assets/js/custom.js
@@ -1,3 +1,36 @@
-const Intranet = {
-    
+let AA = {
+    $menu: document.querySelector(".nav-wrap"),
+
+    toggleMobNav: () => {
+		let $el = document.querySelector(".js-nav-toggler");
+		
+		if( !$el ) {
+			return;
+		}
+
+		$el.addEventListener("click", () => {
+			let $hamburger = document.querySelector("#hamburger");
+
+			if( $hamburger.classList.contains("is-opened") ){
+				AA.helperCloseMobNav();
+			} else {
+				AA.helperOpenMobNav();
+			}
+
+			$hamburger.classList.toggle("is-opened");
+		});
+		/*
+		document.querySelector(".js-back-mob-nav").addEventListener("click", () => {
+			AA.helperOpenMobNav();
+		});*/
+	},
+	helperCloseMobNav: () => {
+		AA.$menu.classList.remove("is-ready", "is-opened");
+	},
+	helperOpenMobNav: () => {
+		AA.$menu.classList.add("is-opened");
+		AA.$menu.classList.remove("is-ready");
+	},
 }
+
+AA.toggleMobNav();
