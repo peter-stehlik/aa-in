9956bb3bb2812b941e28472de6ed76ed03520fe3
Peter Stehlik <peter@inovative.sk>
2022-02-14 11:33:47 +0100

    pripravit vizitku

--- wp-content/themes/wp-dashboard/index.php
+++ wp-content/themes/wp-dashboard/index.php
@@ -1,1 +1,18 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html lang="sk" style="padding: 0; margin: 0;">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aardwark Intranet</title>
+
+    <?php wp_head(); ?>
+</head>
+<body style="padding: 0; margin: 0;">
+    <div style="width: 100%; height: 100vh; display: flex; justify-content: center; align-items: center;">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.svg" width="320" height="240" alt="">
+    </div>
+
+    <?php wp_footer(); ?>
+</body>
+</html>
