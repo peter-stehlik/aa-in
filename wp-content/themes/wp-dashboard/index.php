14bb25c441098f2cf534b9318fb346c6e4b13f71
Peter Stehlik <peter@inovative.sk>
2022-02-22 13:26:42 +0100

    vypis clankov

--- wp-content/themes/wp-dashboard/index.php
+++ wp-content/themes/wp-dashboard/index.php
@@ -12,130 +12,78 @@
 				</form>
 
 				<div class="flex flex-wrap -mx-2 mb-8">
-					<div class="w-full sm:w-1/2 lg:w-2/3 mb-4 px-2 hover:drop-shadow-xl hover:-translate-y-1 transition-transform duration-500">
-						<article class="h-full bg-quaternary p-4 rounded-lg text-white">
-							<figure>
-								<a href="#">
-									<img class="w-full rounded-lg object-cover" src="./assets/img/og-image.jpg" height="192" alt="" style="height: 227px;">
-								</a>
-							</figure>
+					<?php $loop = 1; ?>
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+						<?php if( $loop == 1 ): ?>
+							<div class="w-full sm:w-1/2 lg:w-2/3 mb-4 px-2 hover:drop-shadow-xl hover:-translate-y-1 transition-transform duration-500">
+								<article class="h-full bg-quaternary p-4 rounded-lg text-white">
+									<figure>
+										<a href="<?php the_permalink(); ?>">
+											<?php if( get_the_post_thumbnail() ): ?>
+												<?php the_post_thumbnail("post-thumbnail", ['class' => 'w-full rounded-lg object-cover']) ?>
+											<?php else: ?>
+												<span style="display: flex; justify-content: center; padding: 3rem 0;">
+													<img src="<?php echo get_template_directory_uri(); ?>/assets/img/aardwark-green.svg" width="150" height="120" alt="aardwark">
+												</span>
+											<?php endif; ?>
+										</a>
+									</figure>
 
-							<div class="pt-4">
-								<span class="text-sm text-green"><a class="hover:border-b border-green" href="#">TB info</a></span>
-								<h2 class="text-xl lg:text-3xl"><a class="hover:underline focus:text-green" href="#">Celofiremná akcia už čoskoro.</a></h2>
+									<div class="pt-4">
+										<?php
+											$current_post_categories = get_the_category(); 
+											foreach( $current_post_categories as $category): ?>
+												<span class="text-sm text-green"><a class="hover:border-b border-green" href="<?php echo home_url(); ?>?cat=<?php echo $category->cat_ID; ?>"><?php echo $category->cat_name; ?></a></span>
+											<?php endforeach; ?>
+
+										<h2 class="text-xl lg:text-3xl"><a class="hover:underline focus:text-green" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+									</div>
+								</article>
 							</div>
-						</article>
+
+						<?php elseif( $loop == 2 ): ?>
+							<div class="w-full sm:w-1/2 lg:w-1/3 lg:invisible">
+
+							</div>
+						<?php else: ?>
+							<div class="w-full sm:w-1/2 lg:w-1/3 mb-4 px-2 hover:drop-shadow-xl hover:-translate-y-1 transition-transform duration-500">
+								<article class="h-full <?php if( $loop == 5 || $loop == 7 ): ?> bg-quaternary <?php else: ?> bg-secondary <?php endif; ?> p-4 rounded-lg text-white">
+									<figure>
+										<a href="<?php the_permalink(); ?>">
+											<?php if( get_the_post_thumbnail() ): ?>
+												<?php the_post_thumbnail("medium", ['class' => 'rounded-lg object-cover']) ?>
+											<?php else: ?>
+												<span style="display: flex; justify-content: center; padding: 3rem 0;">
+													<img src="<?php echo get_template_directory_uri(); ?>/assets/img/aardwark-green.svg" width="150" height="120" alt="aardwark">
+												</span>
+											<?php endif; ?>
+										</a>
+									</figure>
+
+									<div class="pt-4">
+										<?php
+											$current_post_categories = get_the_category(); 
+											foreach( $current_post_categories as $category): ?>
+												<span class="text-sm text-green"><a class="hover:border-b border-green" href="<?php echo home_url(); ?>?cat=<?php echo $category->cat_ID; ?>"><?php echo $category->cat_name; ?></a></span>
+											<?php endforeach; ?>
+										
+										<h2 class="text-xl lg:text-3xl"><a class="hover:underline focus:text-green" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+									</div>
+								</article>
+							</div>
+						<?php endif; ?>
+					<?php $loop++; endwhile; ?>
+
+					<div class="w-full py-4">
+						<div class="flex justify-between items-center w-full px-2 mt-8 mb-20">
+							<?php previous_posts_link( 'Predošlé' ); ?>
+							<?php next_posts_link( 'Ďalšie' ); ?>
+						</div>
 					</div>
 
-					<div class="w-full sm:w-1/2 lg:w-1/3 mb-4 px-2 lg:invisible hover:drop-shadow-xl hover:-translate-y-1 transition-transform duration-500">
-						<article class="h-full bg-secondary p-4 rounded-lg text-white">
-							<figure>
-								<a href="#">
-									<img class="rounded-lg" src="./assets/img/caoursel2.jpg" alt="">
-								</a>
-							</figure>
-
-							<div class="pt-4">
-								<span class="text-sm text-green"><a class="hover:border-b border-green" href="#">Raňajky info</a></span>
-								<h2 class="text-xl lg:text-3xl"><a class="hover:underline focus:text-green" href="#">Raňajky v štýle indickej kuchyne.</a></h2>
-							</div>
-						</article>
-					</div>
-
-					<div class="w-full sm:w-1/2 lg:w-1/3 mb-4 px-2 hover:drop-shadow-xl hover:-translate-y-1 transition-transform duration-500">
-						<article class="h-full bg-secondary p-4 rounded-lg text-white">
-							<figure>
-								<a href="#">
-									<img class="rounded-lg" src="./assets/img/caoursel1.jpg" alt="">
-								</a>
-							</figure>
-
-							<div class="pt-4">
-								<span class="text-sm text-green"><a class="hover:border-b border-green" href="#">TB info</a></span>
-								<h2 class="text-xl lg:text-3xl"><a class="hover:underline focus:text-green" href="#">Najbližšie sa stretneme na lezeckej stene.</a></h2>
-							</div>
-						</article>
-					</div>
-
-					<div class="w-full sm:w-1/2 lg:w-1/3 mb-4 px-2 hover:drop-shadow-xl hover:-translate-y-1 transition-transform duration-500">
-						<article class="h-full bg-secondary p-4 rounded-lg text-white">
-							<figure>
-								<a href="#">
-									<img class="rounded-lg" src="./assets/img/caoursel3.jpg" alt="">
-								</a>
-							</figure>
-
-							<div class="pt-4">
-								<span class="text-sm text-green"><a class="hover:border-b border-green" href="#">Dovolenky info</a></span>
-								<h2 class="text-xl lg:text-3xl"><a class="hover:underline focus:text-green" href="#">Celozávodná dovolenka tento rok nebude.</a></h2>
-							</div>
-						</article>
-					</div>
-
-					<div class="w-full sm:w-1/2 lg:w-1/3 mb-4 px-2 hover:drop-shadow-xl hover:-translate-y-1 transition-transform duration-500">
-						<article class="h-full bg-quaternary p-4 rounded-lg text-white">
-							<figure>
-								<a href="#">
-									<img class="rounded-lg" src="./assets/img/caoursel2.jpg" alt="">
-								</a>
-							</figure>
-
-							<div class="pt-4">
-								<span class="text-sm text-green"><a class="hover:border-b border-green" href="#">Office info</a></span>
-								<h2 class="text-xl lg:text-3xl"><a class="hover:underline focus:text-green" href="#">Tento týždeň upratujú devopsáci.</a></h2>
-							</div>
-						</article>
-					</div>
-
-					<div class="w-full sm:w-1/2 lg:w-1/3 mb-4 px-2 hover:drop-shadow-xl hover:-translate-y-1 transition-transform duration-500">
-						<article class="h-full bg-secondary p-4 rounded-lg text-white">
-							<figure>
-								<a href="#">
-									<img class="rounded-lg" src="./assets/img/caoursel3.jpg" alt="">
-								</a>
-							</figure>
-
-							<div class="pt-4">
-								<span class="text-sm text-green"><a class="hover:border-b border-green" href="#">Dovolenky info</a></span>
-								<h2 class="text-xl lg:text-3xl"><a class="hover:underline focus:text-green" href="#">Celozávodná dovolenka tento rok nebude.</a></h2>
-							</div>
-						</article>
-					</div>
-
-					<div class="w-full sm:w-1/2 lg:w-1/3 mb-4 px-2 hover:drop-shadow-xl hover:-translate-y-1 transition-transform duration-500">
-						<article class="h-full bg-quaternary p-4 rounded-lg text-white">
-							<figure>
-								<a href="#">
-									<img class="rounded-lg" src="./assets/img/caoursel2.jpg" alt="">
-								</a>
-							</figure>
-
-							<div class="pt-4">
-								<span class="text-sm text-green"><a class="hover:border-b border-green" href="#">Raňajky info</a></span>
-								<h2 class="text-xl lg:text-3xl"><a class="hover:underline focus:text-green" href="#">Raňajky v štýle indickej kuchyne.</a></h2>
-							</div>
-						</article>
-					</div>
-
-					<div class="w-full sm:w-1/2 lg:w-1/3 mb-4 px-2 hover:drop-shadow-xl hover:-translate-y-1 transition-transform duration-500">
-						<article class="h-full bg-secondary p-4 rounded-lg text-white">
-							<figure>
-								<a href="#">
-									<img class="rounded-lg" src="./assets/img/caoursel2.jpg" alt="">
-								</a>
-							</figure>
-
-							<div class="pt-4">
-								<span class="text-sm text-green"><a class="hover:border-b border-green" href="#">Office info</a></span>
-								<h2 class="text-xl lg:text-3xl"><a class="hover:underline focus:text-green" href="#">Tento týždeň upratujú devopsáci.</a></h2>
-							</div>
-						</article>
-					</div>
-				</div>
-
-				<div class="flex justify-between items-center py-4 mb-20">
-					<a href="#">Predošlé</a>
-					<a href="#">Ďalšie</a>
+					<?php else : ?>
+						<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+					<?php endif; ?>
 				</div>
 
                 <?php get_footer("html"); ?>
