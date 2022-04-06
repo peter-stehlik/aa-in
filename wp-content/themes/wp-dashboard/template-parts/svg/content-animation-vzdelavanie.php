872c0355091b27b972e16f78f14f89c6e991510f
Peter Stehlik <peter@inovative.sk>
2022-04-06 13:47:13 +0200

    svg ikona pridana

---
+++ wp-content/themes/wp-dashboard/template-parts/svg/content-animation-vzdelavanie.php
@@ -0,0 +1,81 @@
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 200 120" style="enable-background:new 0 0 200 120;" xml:space="preserve">
+<style type="text/css">
+	.st0{fill:#6846A0;}
+	.st1{fill:none;stroke:#82D2D2;stroke-linejoin:round;stroke-miterlimit:10;}
+	.st2{fill:#CCA0D7;}
+	.st3{fill:none;stroke:#6846A0;stroke-width:2;stroke-miterlimit:10;}
+	.st4{fill:none;stroke:#6846A0;stroke-miterlimit:10;}
+	.st5{fill:#CCA0D7;stroke:#6846A0;stroke-width:2;stroke-miterlimit:10;}
+	.st6{fill:#FFFFFF;stroke:#6846A0;stroke-miterlimit:10;}
+	.st7{fill:none;stroke:#82D2D2;stroke-miterlimit:10;}
+	.st8{fill:#82D2D2;}
+	.st9{fill:none;stroke:#CCA0D7;stroke-width:2.0312;stroke-miterlimit:10;}
+	.st10{fill:#A06DC0;}
+	.st11{fill-rule:evenodd;clip-rule:evenodd;fill:none;}
+	.st12{fill:#FFFFFF;}
+	.st13{fill:none;stroke:#6846A0;stroke-width:0.9509;stroke-miterlimit:10;}
+	.st14{fill:#71519D;}
+	.st15{fill:none;stroke:#82D2D2;stroke-width:2;stroke-miterlimit:10;}
+	.st16{fill:none;stroke:#CEA8CD;stroke-width:2;stroke-miterlimit:10;}
+	.st17{fill:none;stroke:#FFFFFF;stroke-miterlimit:10;}
+	.st18{fill:none;stroke:#A06DC0;stroke-miterlimit:10;}
+	.st19{fill:none;}
+	.st20{fill-rule:evenodd;clip-rule:evenodd;fill:#FFFFFF;}
+	.st21{fill:none;stroke:#6846A0;stroke-width:13;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}
+	.st22{fill:#FFFFFF;stroke:#6846A0;stroke-width:2;stroke-linejoin:round;stroke-miterlimit:10;}
+	.st23{fill:none;stroke:#6846A0;stroke-width:2;stroke-linejoin:round;stroke-miterlimit:10;}
+	.st24{fill:#6846A0;stroke:#6846A0;stroke-width:2;stroke-miterlimit:10;}
+	.st25{fill:none;stroke:#CCA0D7;stroke-width:0.6718;stroke-miterlimit:10;}
+	.st26{fill:none;stroke:#6846A0;stroke-width:0.5673;stroke-miterlimit:10;}
+	.st27{fill:none;stroke:#6846A0;stroke-width:1.2874;stroke-miterlimit:10;}
+	.st28{fill:none;stroke:#A06DC0;stroke-width:1.0872;stroke-miterlimit:10;}
+	.st29{fill:none;stroke:#82D2D2;stroke-width:1.0872;stroke-miterlimit:10;}
+	.st30{fill:none;stroke:#82D2D2;stroke-width:0.6437;stroke-miterlimit:10;}
+	.st31{fill:none;stroke:#A06DC0;stroke-width:2;stroke-miterlimit:10;}
+	.st32{fill-rule:evenodd;clip-rule:evenodd;fill:#6846A0;}
+	.st33{clip-path:url(#SVGID_2_);fill-rule:evenodd;clip-rule:evenodd;fill:#FFFFFF;}
+	.st34{fill:none;stroke:#A06DC0;stroke-linejoin:round;stroke-miterlimit:10;}
+	.st35{fill:#FFFFFF;stroke:#6846A0;stroke-width:2;stroke-miterlimit:10;}
+	.st36{fill:none;stroke:#82D2D2;stroke-linecap:round;stroke-miterlimit:10;}
+	.st37{fill:#CCA0D7;stroke:#CCA0D7;stroke-width:2;stroke-miterlimit:10;}
+	.st38{fill:none;stroke:#CCA0D7;stroke-miterlimit:10;}
+</style>
+<path class="st37" d="M24.6,11.8c-4.4,0-7.9,3.6-7.9,7.9v60.2c0-4.4,3.6-7.9,7.9-7.9h56V11.8H24.6z"/>
+<path class="st35" d="M80.6,72h-56c-4.4,0-7.9,3.6-7.9,7.9v0c0,4.4,3.6,7.9,7.9,7.9h56V72z"/>
+<path class="st6" d="M165.5,25.5H73.1c-5.5,0-9.9,4.4-9.9,9.9V98c0,2.6,2.1,4.7,4.7,4.7h102.9c2.6,0,4.7-2.1,4.7-4.7V35.4
+	C175.4,29.9,170.9,25.5,165.5,25.5z"/>
+<path class="st0" d="M184.4,94.9H54.2l0,0.4c0.1,4.5,3.7,8,8.2,8h113.8C180.8,103.3,184.5,99.5,184.4,94.9L184.4,94.9z"/>
+<polygon class="st8" points="120.6,52.9 113,56.9 114.5,48.4 108.4,42.4 116.8,41.2 120.6,33.5 124.4,41.2 132.9,42.4 126.8,48.4 
+	128.2,56.9 "/>
+<polygon class="st8" points="150.6,52.9 143,56.9 144.5,48.4 138.3,42.4 146.8,41.2 150.6,33.5 154.4,41.2 162.9,42.4 156.7,48.4 
+	158.2,56.9 "/>
+<polygon class="st8" points="180.2,52.9 172.6,56.9 174.1,48.4 167.9,42.4 176.4,41.2 180.2,33.5 184,41.2 192.5,42.4 186.4,48.4 
+	187.8,56.9 "/>
+<g>
+	<line class="st4" x1="126.1" y1="84.1" x2="118" y2="84.2"/>
+	<path class="st3" d="M98.4,86.5L83.6,67.9c-1.5-1.9-4.1-2.8-6.4-1.9c-1.8,0.7-3.2,2.2-3.4,4.3c0,0.2,0,0.4,0,0.6"/>
+	<path class="st3" d="M143.2,80.6l-10.8-13.6c-1.6-2-4.3-2.9-6.7-2c-1.9,0.7-3.3,2.3-3.5,4.5c0,0.2,0,0.4,0,0.7"/>
+	<circle class="st6" cx="136" cy="87" r="10.2"/>
+	<circle class="st6" cx="107.5" cy="88" r="11.1"/>
+</g>
+<g>
+	<g>
+		<line class="st38" x1="77.5" y1="46.6" x2="90.1" y2="46.6"/>
+		<line class="st4" x1="77.5" y1="58.5" x2="93.6" y2="58.5"/>
+		<line class="st4" x1="77.5" y1="34.8" x2="104.3" y2="34.8"/>
+		<line class="st18" x1="77.5" y1="52.5" x2="100.4" y2="52.5"/>
+	</g>
+	<line class="st15" x1="77.5" y1="40.7" x2="96.5" y2="40.7"/>
+</g>
+<g>
+	<g>
+		<g>
+			<path class="st7" d="M35.7,79.1c0-20.8,16.6-37.7,37.2-38.3"/>
+			<g>
+				<polygon class="st8" points="69.8,45.2 69.1,44.5 72.5,40.9 68.7,37.8 69.4,37 74,40.9 				"/>
+			</g>
+		</g>
+	</g>
+</g>
+</svg>
